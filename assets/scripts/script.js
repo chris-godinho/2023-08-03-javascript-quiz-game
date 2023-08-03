acb97206b6efe40afa932a4ded68b67a01f937fe
kristbg <kristbg@gmail.com>
2023-08-03 17:27:03 -0400

    Added questions to pool, Fixed initials form

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -17,20 +17,30 @@ var storedHighScoreList = [];
 var enteredInitials;
 var newScore;
 var correctAnswer = 0;
-var firstQuestion = true;
+var currentQuestion = 0;
 
-var questionPool = [
-["Inside which HTML element do we put the JavaScript?", "<script>", "<scripting>", "<js>", "<javascript>", 1],
-['What is the correct JavaScript syntax to change the content of the HTML element below?\n<p id="demo">This is a demonstration.</p>', 'document.getElementByName("p").innerHTML = "Hello World!";', 'document.getElement("p").innerHTML = "Hello World!";', 'document.getElementById("demo").innerHTML = "Hello World!";', '#demo.innerHTML = "Hello World!";', 3],
-['Where is the correct place to insert a JavaScript?', 'The <head> section', 'The <body> section', 'The <javascript> section', 'Both the <head> section and the <body> section are correct', 2],
-['What is the correct syntax for referring to an external script called "xxx.js"?', '<script src="xxx.js">', '<script href="xxx.js">', '<script name="xxx.js">', '<script external="xxx.js">', 1],
-['The external JavaScript file must contain the <script> tag.', 'False', 'True', 'Both', 'Neither', 1],
+var cQuestionPool = [
+['Inside which HTML element do we put the JavaScript?', '&lt;script&gt;', '&lt;scripting&gt;', '&lt;js&gt;', '&lt;javascript&gt;', 1],
+['What is the correct JavaScript syntax to change the content of the HTML element &lt;p id="demo"&gt;This is a demonstration.&lt;/p&gt;?', 'document.getElementByName("p").innerHTML = "Hello World!";', 'document.getElement("p").innerHTML = "Hello World!";', 'document.getElementById("demo").innerHTML = "Hello World!";', '#demo.innerHTML = "Hello World!";', 3],
+['Where is the correct place to insert a JavaScript?', 'The &lt;head&gt; section', 'The &lt;body&gt; section', 'The &lt;javascript&gt; section', 'Both the &lt;head&gt; section and the &lt;body&gt; section are correct', 2],
+['What is the correct syntax for referring to an external script called "xxx.js"?', '&lt;script src="xxx.js"&gt;', '&lt;script href="xxx.js"&gt;', '&lt;script name="xxx.js"&gt;', '&lt;script external="xxx.js"&gt;', 1],
+['The external JavaScript file must contain the &lt;script&gt; tag.', 'False', 'True', 'Both', 'Neither', 1],
 ['How do you write "Hello World" in an alert box?', 'alertBox("Hello World");', 'msgBox("Hello World");', 'msg("Hello World");', 'alert("Hello World");', 4],
-['How do you create a function in JavaScript?', 'function myFunction()', 'function:myFunction()', 'function = myFunction()', 'function >> myFunction()', 1],
+['How do you create a function in JavaScript?', 'function myFunction()', 'function:myFunction()', 'function = myFunction()', 'function &gt;&gt; myFunction()', 1],
 ['How do you call a function named "myFunction"?', 'myFunction()', 'call myFunction()', 'call function myFunction()', 'call internal function myFunction()', 1],
 ['How do you write an IF statement in JavaScript?', 'if i == 5 then', 'if i = 5 then', 'if (i == 5)', 'if i = 5', 3],
-['How do you write an IF statement for executing some code if "i" is NOT equal to 5?', 'if i =! 5 then', 'if i <> 5', 'if (i <> 5)', 'if (i != 5)', 4]
+['How do you write an IF statement for executing some code if "i" is NOT equal to 5?', 'if i =! 5 then', 'if i &lt;&gt; 5', 'if (i &lt;&gt; 5)', 'if (i != 5)', 4],
+['How does a WHILE loop start?', 'while (i &lt;= 10; i++)', 'while (i &lt;= 10)', 'while i = 1 to 10', 'while loop (i &lt;= 10)', 2],
+['How does a FOR loop start?', 'for (i = 0; i &lt;= 5)', 'for (i &lt;= 5; i++)', 'for i = 1 to 5', 'for (i = 0; i &lt;= 5; i++)', 4],
+['How can you add a comment in a JavaScript?', "'This is a comment", '&lt;!--This is a comment--&gt;', '//This is a comment', '%-This is a comment', 3],
+['What is the correct way to write a JavaScript array?', 'var colors = 1 = ("red"), 2 = ("green"), 3 = ("blue")', 'var colors = (1:"red", 2:"green", 3:"blue")', 'var colors = ["red", "green", "blue"]', 'var colors = "red", "green", "blue"', 3],
+['How do you round the number 7.25, to the nearest integer?', 'Math.rnd(7.25)', 'round(7.25)', 'Math.round(7.25)', 'rnd(7.25)', 3],
+['How do you find the number with the highest value of x and y?', 'ceil(x,y)', 'top(x,y)', 'Math.max(x,y)', 'Math.ceil(x,y)', 3],
+['JavaScript is the same as Java.', 'True', 'False', 'Both', 'Neither', 2],
+['Which event occurs when the user clicks on an HTML element?', 'onclick', 'onchange', 'onmouseover', 'onmouseclick', 1],
+['How do you declare a JavaScript variable?', 'variable carName', 'v carName', 'var carName', 'new variable(carName)', 3]
 ];
+var questionPool;
 var nextQuestion;
 
 // Storing each screen ID in a different element for quick access
@@ -69,22 +79,25 @@ function displayFinalScore() {
 
 function saveInitials() {
   enteredInitials = document.getElementById("initials").value;
-  highScoreList.push(enteredInitials + " (" + currentScore + ")");
-  localStorage.setItem("highscores", highScoreList);
-  storedHighScoreList = localStorage.getItem("highscores").split(",");
-  document.getElementById("high-score-list").innerHTML = "";
-  for (i = 0; i < storedHighScoreList.length; i++) {
-    newScore = document.createElement("li");
-    newScore.innerHTML = storedHighScoreList[i];
+  if (enteredInitials === "") {
+    alert("Field cannot be blank!\nSurely your name contains initials?");
+  } else {
+    highScoreList.push(enteredInitials + " (" + currentScore + ")");
+    localStorage.setItem("highscores", highScoreList);
+    storedHighScoreList = localStorage.getItem("highscores").split(",");
+    document.getElementById("high-score-list").innerHTML = "";
+    for (i = 0; i < storedHighScoreList.length; i++) {
+      newScore = document.createElement("li");
+      newScore.innerHTML = storedHighScoreList[i];
     document.getElementById("high-score-list").appendChild(newScore);
+    }
+    displayHighScores();
+    document.getElementById("initials").value = "";
   }
-  displayHighScores();
-  console.log(highScoreList);
-  console.log(localStorage.getItem("highscores"));
-  document.getElementById("initials").value = "";
 }
 
 function displayHighScores() {
+  clearInterval(activeTimer);
   startEl.style.display = "none";
   quizEl.style.display = "none";
   timerEl.style.display = "none";
@@ -108,14 +121,10 @@ function decreaseTimer() {
 }
 
 function displayFeedback() {
-  console.log("correctAnswer -> " + correctAnswer);
-  console.log(correctAnswer.typeof);
   if (correctAnswer) {
-    console.log("CORRECT ANSWER");
     document.getElementById("feedback").innerHTML = "Correct!";
     document.getElementById("feedback").style.color = "#2AB8CB";
   } else {
-    console.log("INCORRECT ANSWER");
     document.getElementById("feedback").innerHTML = "Incorrect!";
     document.getElementById("feedback").style.color = "#FE3434";
   }
@@ -175,32 +184,35 @@ function selectOptionD() {
 }
 
 function getNextQuestion() {
-  if (questionPool.length === 0) {
-    displayFinalScore();
-  } else {
-    if (firstQuestion) {
-      firstQuestion = false;
-    } else {
-      displayFeedback();
+  console.log(questionPool);
+  if (currentQuestion > 0) {
+    displayFeedback();
+    if (currentQuestion >= 10) {
+      clearInterval(activeTimer);
+      displayFinalScore();
+      return;
     }
-    nextQuestion = questionPool.splice(Math.floor(Math.random() * questionPool.length), 1);
-    document.getElementById("question").innerHTML = nextQuestion[0][0];
-    document.getElementById("op-a").innerHTML = nextQuestion[0][1];
-    document.getElementById("op-b").innerHTML = nextQuestion[0][2];
-    document.getElementById("op-c").innerHTML = nextQuestion[0][3];
-    document.getElementById("op-d").innerHTML = nextQuestion[0][4];
   }
-
-}
+  nextQuestion = questionPool.splice(Math.floor(Math.random() * questionPool.length), 1);
+  document.getElementById("question").innerHTML = nextQuestion[0][0];
+  document.getElementById("op-a").innerHTML = nextQuestion[0][1];
+  document.getElementById("op-b").innerHTML = nextQuestion[0][2];
+  document.getElementById("op-c").innerHTML = nextQuestion[0][3];
+  document.getElementById("op-d").innerHTML = nextQuestion[0][4];
+  currentQuestion++;
+  }
 
 function startGame() {
   currentScore = 0;
-  document.getElementById("timer-counter").innerHTML = 90;
+  currentQuestion = 0;
+  questionPool = [...cQuestionPool];
+  document.getElementById("timer-counter").innerHTML = 60;
   activeTimer = window.setInterval(decreaseTimer, 1000);
   displayQuiz();
   getNextQuestion();
 }
 
+// Adding event listeners to buttons
 startButton.addEventListener("click", startGame);
 highScores.addEventListener("click", displayHighScores);
 confirmInitials.addEventListener("click", saveInitials);
